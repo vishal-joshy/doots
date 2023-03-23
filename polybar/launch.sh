a162ca272148fb1bd7a12960f3cfc1d9caffd799
vishal-joshy <1812-vishal@protonmail.com>
2023-03-23 23:01:26 +0530

    add polybar mpv configs

---
+++ polybar/launch.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# # Terminate already running bar instances
+# # If all your bars have ipc enabled, you can use
+# polybar-msg cmd quit
+# # Otherwise you can use the nuclear option:
+# # killall -q polybar
+#
+# # Launch bar1 and bar2
+# echo "---" | tee -a /tmp/polybar1.log
+# polybar & disown
+#
+# echo "Bars launched..."
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+killall -q polybar
+
+# Launch Polybar
+polybar bar1 2>&1 | tee -a /tmp/polybar.log &
+disown
+
+echo "Bars launched..."
